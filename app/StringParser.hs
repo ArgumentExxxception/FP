bde5f66b7d9ca03f963306a5de33410f53441591
Алмаз Ахметшин <almazahmetsin@MacBook-Pro-Almaz.local>
2024-12-18 14:32:32 +0300

    1) инициализировал cabal проект 2) перенес в проект все существующие файлы 3) в стринг парсере добавил перевод каретки CR 4) дополнил операции над стэком

---
+++ app/StringParser.hs
@@ -0,0 +1,45 @@
+module StringParser( parsePrintString, printString ) where
+
+import Data.List (isInfixOf)
+
+parsePrintString :: String -> IO ()
+parsePrintString input = 
+    if take 3 input == ". \"" && last input == '"'
+        then putStrLn input
+    else if take 1 input == "#" 
+        then return ()
+    else putStrLn "Неверный формат"
+
+printString :: [String] -> IO()
+printString [] = return ()
+printString (line:rest)
+    | "CR" `isInfixOf` line =
+        processStringWithCR line
+    | take 1 line == "#" = 
+        printString rest
+    | take 3 line == ". \"" && last line == '"' = do
+        putStrLn (init (drop 3 line))
+        printString rest
+    | otherwise = do
+        putStrLn "Неверный формат"
+        printString rest
+
+
+processStringWithCR :: String -> IO ()
+processStringWithCR "" = return ()
+processStringWithCR str
+    | "CR" `isInfixOf` str = do
+        let (before, after) = splitByCR str
+        putStrLn (trim before)
+        processStringWithCR (trim after)
+    | otherwise = putStrLn (trim str)
+
+splitByCR :: String -> (String, String)
+splitByCR str =
+    let (before, rest) = span (/= 'C') str
+    in if take 2 rest == "CR"
+        then (before, drop 2 rest)
+        else (str, "")
+
+trim :: String -> String
+trim = reverse . dropWhile (== ' ') . reverse . dropWhile (== ' ')
