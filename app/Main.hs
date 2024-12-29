82620589b55d044c3b2b2cb3102c036174956950
Алмаз Ахметшин <almazahmetsin@MacBook-Pro-Almaz.local>
2024-12-29 18:19:42 +0300

    1) Докинул рандомизированный тест

--- app/Main.hs
+++ app/Main.hs
@@ -8,7 +8,8 @@ import MainParser (programParser)
 import Text.Megaparsec (runParser, errorBundlePretty)
 import Test.Tasty
 import Test.Tasty.HUnit
-import Test.QuickCheck
+import Test.Tasty.QuickCheck as QC
+import System.IO.Unsafe (unsafePerformIO)
 
 type Stack = [Int]
 type StackMachine = State Stack
@@ -20,57 +21,19 @@ main :: IO ()
 main = defaultMain tests
 
 tests :: TestTree
-tests = testGroup "Colon Language Tests"
-    [ testCase "Arithmetic operation: 1 2 +" $ do
-        result <- runTest "1 2 +"
-        result @?= Right [3]
-    , testCase "Arithmetic operation: 3 4 *" $ do
-        result <- runTest "3 4 *"
-        result @?= Right [12]
-    , testCase "Arithmetic operation: 12 5 MOD" $ do
-        result <- runTest "-12 5 MOD"
-        result @?= Right [2]
-    , testCase "Arithmetic operation: 5 2 + 10 *" $ do
-        result <- runTest "5 2 + 10 *"
-        result @?= Right [70]
-    , testCase "StackOperation: 1 2 3 4 DUP" $ do
-        result <- runTest "1 2 3 4 DUP"
-        result @?= Right [4, 4, 3, 2, 1]
-    , testCase "StackOperation: 1 2 3 4 DROP" $ do
-        result <- runTest "1 2 3 4 DROP"
-        result @?= Right [3, 2, 1]
-    , testCase "StackOperation: 1 2 3 4 SWAP" $ do
-        result <- runTest "1 2 3 4 SWAP"
-        result @?= Right [3,4,2,1]
-    , testCase "StackOperation: 1 2 3 4 OVER" $ do
-        result <- runTest "1 2 3 4 OVER"
-        result @?= Right [3,4,3,2,1]
-    , testCase "Compairing Operation: 3 4 =" $ do
-        result <- runTest "3 4 ="
-        result @?= Right [0]
-    , testCase "Compairing Operation: 5 5 =" $ do
-        result <- runTest "5 5 ="
-        result @?= Right [-1]    
-    , testCase "Compairing Operation: 3 4 >" $ do
-        result <- runTest "3 4 >"
-        result @?= Right [-1]
-    , testCase "Input-Output: 1 2 . . 3 . 4" $ do
-        result <- runTest "1 2 . . 3 . 4"
-        result @?= Right [4]
-    , testCase "Condition: 0 IF 1 2 ELSE 3 4 THEN" $ do
-        result <- runTest "0 IF 1 2 ELSE 3 4 THEN"
-        result @?= Right [4,3]
-    , testCase "Condition: 1 IF 1 2 ELSE 3 4 THEN" $ do
-        result <- runTest "1 IF 1 2 ELSE 3 4 THEN"
-        result @?= Right [2,1]
-    , testCase "DO LOOP: DO 1 2 3 DROP 0 LOOP" $ do
-        result <- runTest "DO 1 2 3 DROP 0 LOOP"
-        result @?= Right [2,1]
+tests = testGroup "Простой рандомизированный тест"
+    [ QC.testProperty "Сложение двух рандомных чисел" $
+        \x y -> testAddition x y
     ]
 
-runTest :: String -> IO (Either String Stack)
-runTest input = case runParser programParser "" input of
-    Left err -> return $ Left $ errorBundlePretty err
-    Right commands -> do
-        result <- execStateT (mapM_ executeCommand commands) []
-        return $ Right result
+testAddition :: Int -> Int -> Bool
+testAddition x y = unsafePerformIO $ do
+    let input = show x ++ " " ++ show y ++ " +"
+    if x < 0 || y < 0
+        then return True
+    else
+        case runParser programParser "" input of
+            Left _ -> return False
+            Right commands -> do
+                result <- execStateT (mapM_ executeCommand commands) []
+                return $ result == [x + y]
